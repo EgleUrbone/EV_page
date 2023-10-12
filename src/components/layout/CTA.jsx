7f8b817c210c14bdfd90e7287125e52e75f9a8b2
Egle <132908296+EgleUrbone@users.noreply.github.com>
2023-10-12 11:46:40 +0300

    colors + fonts + background

--- src/components/layout/CTA.jsx
+++ src/components/layout/CTA.jsx
@@ -1,9 +1,14 @@
+import css from './CTA.module.css';
 
 export default function CTA() {
   return (
-    <div>
-      <h1>Įgyvendinkime jūsų viziją kurdami kartu. Vizijas verčiame realybe..</h1>
-      <button>Susisiekime</button>
+    <div className={css.cta}>
+      <div className={css.textCont}>
+        <h1 className={css.mainLine}>
+          Įgyvendinkime jūsų viziją kurdami kartu. Vizijas verčiame realybe..
+        </h1>
+        <button className={css.btn}>Susisiekime</button>
+      </div>
     </div>
-  )
+  );
 }
